037c46ac8807ab3ad38b905e840c0c17bbf4f953
Daniil <daniil@vivaldi.net>
2023-12-26 17:40:17 +0400

    feat(script): update usage, write readme

--- README.md
+++ README.md
@@ -1,8 +1,66 @@
 # Change the form manually
 
-1. Set the `target` 
+1. Set the `target`
 2. TODO
 
 # Inject the script
 
-1. TODO
+### Minimum configuration
+
+```html
+
+<script src="https://example.com/mindall-crm-plugin.js"></script>
+<script>
+    window.MINDALL_CRM.init({
+        inputs: {
+            // Names of fields in the form. (Required)
+            firstname: 'wpforms[fields][0][first]',
+            lastname: 'wpforms[fields][0][last]',
+            phone: 'wpforms[fields][3]',
+            email: 'wpforms[fields][1]',
+            notes: 'wpforms[fields][2]',
+        },
+        hiddenInputs: {
+            // Organization ID in Mindall CRM. (Required)
+            orgId: "dd6175f2-ad36-46a8-bfa6-31d0b2f49cff",
+        },
+    })
+</script>
+```
+
+### Available configuration
+```html
+
+<script src="https://example.com/mindall-crm-plugin.js"></script>
+<script>
+    window.MINDALL_CRM.init({
+        // Url to send a request to.
+        // Default: 'https://crm.mindall.co/api/api/lead/create/byExternalForm'
+        url: 'https://example.com/send-the-request-here',
+
+        // Whether to resubmit a form or not.
+        // Default: false
+        submitDefaultForm: true,
+
+        inputs: {
+            firstname: 'wpforms[fields][0][first]',
+            lastname: 'wpforms[fields][0][last]',
+            phone: 'wpforms[fields][3]',
+            email: 'wpforms[fields][1]',
+            notes: 'wpforms[fields][2]',
+        },
+
+        hiddenInputs: {
+            orgId: 'a71de314-8041-4c86-a7b4-9ad22c37fe9b',
+
+            // Lead Source. (acceptable values enumerated in lead_source type in PostgreSQL)
+            // Default: 'website'
+            source: 'landing',
+
+            // Lead Source ID.
+            // Default: title value of the current page
+            sourceId: 'Best Landing #29834',
+        },
+    })
+</script>
+```
--- inject.js
+++ inject.js
@@ -1,22 +1,20 @@
 /// Configuration
 
 let config = {
-    url: 'https://{domain}/api/lead/create/byExternalForm',
+    url: 'https://crm.mindall.co/api/api/lead/create/byExternalForm',
+    submitDefaultForm: false,
 
     inputs: {
-        firstname: 'firstName',
-        lastname: 'lastName',
-        email: 'email',
-        phone: 'phone',
-        notes: 'notes',
+        firstname: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        notes: '',
     },
 
     hiddenInputs: {
-        // Required
-        orgId: '{uuid}',
-        sourceId: '{page-title}',
-
-        // Optional
+        orgId: '',
+        sourceId: '',
         source: '',
     },
 }
@@ -56,24 +54,66 @@ window.saveLead = async (event) => {
     apiCall(data)
         .then(async response => {
             if (!response.ok) {
-                const body = await response.text()
-                console.error(body)
-
                 const errorMessage = response.statusText || 'Request failed'
                 return Promise.reject(errorMessage)
             }
 
-            event.target.submit()
+            if (config.submitDefaultForm) {
+                event.target.submit()
+            }
         })
         .catch(err => alert(err))
 }
 
 function inject() {
+    if (document.forms.length === 0) {
+        console.error('[Mindall CRM] Plugin has not found any forms')
+        return
+    }
+
+    if (!config.hiddenInputs?.sourceId) {
+        setDefaultSourceId()
+    }
+
     Array.from(document.forms).forEach(
         form => form.setAttribute('onsubmit', 'saveLead(event)')
     )
+
+    console.info('[Mindall CRM] Successfully installed')
 }
 
-window.onload = () => {
-    inject()
+function updateConfig(defaultValue, newValue) {
+    for (const newKey in defaultValue) {
+        const field = newValue[newKey] ?? undefined
+
+        if (field === undefined) {
+            continue
+        }
+
+        if (typeof field === 'object' && !Array.isArray(field) && field !== null) {
+            defaultValue[newKey] = updateConfig(defaultValue[newKey], field)
+        } else {
+            defaultValue[newKey] = field
+        }
+    }
+
+    return defaultValue
+}
+
+function setDefaultSourceId() {
+    console.info(`[Mindall CRM] Setting Source ID from title: ${document.title}`)
+    config.hiddenInputs.sourceId = document.title
+}
+
+window.MINDALL_CRM = {
+    init(userConfig) {
+        if (!userConfig || !userConfig.hiddenInputs?.orgId) {
+            console.error('[Mindall CRM] Plugin is not injected due to missing configuration')
+            return
+        }
+
+        config = updateConfig({...config}, userConfig)
+
+        window.onload = inject
+    }
 }
